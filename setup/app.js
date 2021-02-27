80161d1403c2cdbdd9dee4a7bdb26c1d946f8310
chu-young <ttheko101@gmail.com>
2021-02-27 14:52:35 +0200

    finished tabs

---
+++ setup/app.js
@@ -0,0 +1,25 @@
+// declare variables
+
+const btns = document.querySelectorAll('.tab-btn');
+
+const about = document.querySelector('.about');
+
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function(e){
+const id = e.target.dataset.id;
+if(id){
+    //revove active class from other buttons
+    btns.forEach(function(btn){
+        btn.classList.remove('active');
+        e.target.classList.add('active');
+    });
+    // hide other contents and show the one with matching id
+    articles.forEach(function(article){
+        article.classList.remove('active');
+
+    });
+    const element = document.getElementById(id);
+    element.classList.add('active');
+}
+});
